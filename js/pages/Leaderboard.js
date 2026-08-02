9d37704abd38711ee7743a73ab6db87921d120cc
1FireXY1 <50220934+1FireXY1@users.noreply.github.com>
2026-08-01 22:01:37 -0400

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -38,8 +38,6 @@ export default {
                     />
                     <button v-if="search" class="clear-btn" @click="search = ''">Clear</button>
                 </div>
-                <p>  </p>
-                <p>  </p>
                     <table class="board">
                         <!-- iterate over filteredLeaderboard instead of leaderboard -->
                         <tr v-for="(ientry, i) in filteredLeaderboard" :key="ientry.user">
