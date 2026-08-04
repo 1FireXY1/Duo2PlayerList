2b464b5d2516aab1d331ddcf60a37b6a8434b504
1FireXY1 <50220934+1FireXY1@users.noreply.github.com>
2026-08-03 23:13:53 -0400

    Add 'Globed' field to level display

--- js/pages/List.js
+++ js/pages/List.js
@@ -68,6 +68,10 @@ export default {
                             <p>{{ level.id }}</p>
                         </li>
                         <li>
+                            <div class="type-title-sm">Globed</div>
+                            <p>{{ level.globed }}</p>
+                        </li>
+                        <li>
                             <div class="type-title-sm">Password</div>
                             <p>{{ level.password || 'Free to Copy' }}</p>
                         </li>
