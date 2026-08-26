fadd16eea26a0b76eda9f990003bc71ffff37e7c
1FireXY1 <50220934+1FireXY1@users.noreply.github.com>
2026-08-26 07:40:46 -0400

    Implement fetchfullList to load and sort levels
    Added fetchfullList function to retrieve and sort levels from JSON files.

--- js/content.js
+++ js/content.js
@@ -67,6 +67,37 @@ export async function fetchLegacyList() {
     }
 }
 
+export async function fetchfullList() {
+    const listResult = await fetch(`${dir}/_fulllist.json`);
+    try {
+        const list = await listResult.json();
+        return await Promise.all(
+            list.map(async (path, rank) => {
+                const levelResult = await fetch(`${dir}/${path}.json`);
+                try {
+                    const level = await levelResult.json();
+                    return [
+                        {
+                            ...level,
+                            path,
+                            records: level.records.sort(
+                                (a, b) => b.percent - a.percent,
+                            ),
+                        },
+                        null,
+                    ];
+                } catch {
+                    console.error(`Failed to load level #${rank + 1} ${path}.`);
+                    return [null, path];
+                }
+            }),
+        );
+    } catch {
+        console.error(`Failed to load full list.`);
+        return null;
+    }
+}
+
 export async function fetchEditors() {
     try {
         const editorsResults = await fetch(`${dir}/_editors.json`);
